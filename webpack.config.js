7fc1801155f74d8b5c87eed5c04fd671dba28559
Tatyana Fomina <tayna-anita@mail.ru>
2020-07-06 15:52:10 +0300

    добавила TsconfigPathsPlugin для алиасов

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/index.tsx"),
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
