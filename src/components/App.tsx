4186577794b0ecd9f6c0ef9cc9465374e2ea81b6
Tatyana Fomina <tayna-anita@mail.ru>
2020-07-06 17:28:44 +0300

    Перенесла роуты в App компонент

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { Routes } from "routes";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { GlobalFeed } from "pages/globalFeed";
+import { Article } from "pages/article";
 
 export const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={GlobalFeed} exact />
+        <Route path="/articles/:slug" component={Article} exact />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 };
