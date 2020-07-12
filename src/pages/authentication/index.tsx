0af7d3cc2dc0b84be0e30ed89c922aec1e0455be
Tatyana Fomina <tayna-anita@mail.ru>
2020-07-12 22:20:36 +0300

    Переделала разметку на странице Authentication

--- src/pages/authentication/index.tsx
+++ src/pages/authentication/index.tsx
@@ -4,8 +4,8 @@ import { LoginForm } from "components/LoginForm";
 
 export const Authentication: React.FC = () => {
   return (
-    <div className="auth-page py-5">
-      <div className="container page">
+    <div className="py-5">
+      <div className="container">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-center">Login</h1>
