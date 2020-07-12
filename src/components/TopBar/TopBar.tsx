4c4a26c01d286c264c15a74a419705a2a3e9ce3c
Tatyana Fomina <tayna-anita@mail.ru>
2020-07-12 22:12:46 +0300

    Переделала разметку в компоненте TopBar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -3,12 +3,12 @@ import { Link, NavLink } from "react-router-dom";
 
 export const TopBar: React.FC = () => {
   return (
-    <nav className="navbar navbar-light bg-success">
+    <nav className="navbar navbar-light bg-success px-0">
       <div className="container">
         <Link to="/" className="navbar-brand text-white h1 mb-0">
           Medium
         </Link>
-        <ul className="nav navbar pull-xs-right">
+        <ul className="nav navbar">
           <li className="nav-item">
             <NavLink
               to="/"
