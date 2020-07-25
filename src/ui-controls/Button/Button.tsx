1fbedc03ba40b214c2160aaa7791abd1a1ff7378
Tatyana Fomina <tayna-anita@mail.ru>
2020-07-25 22:03:37 +0300

    Добавила свойство disabled к Button

--- src/ui-controls/Button/Button.tsx
+++ src/ui-controls/Button/Button.tsx
@@ -7,21 +7,24 @@ type Props = {
   text: string;
   type: ButtonType;
   className?: string;
+  disabled: boolean;
   onClick?: (e: React.FormEvent<HTMLButtonElement>) => void;
 } & typeof defaultProps;
 
 const defaultProps = {
   type: "button" as ButtonType,
+  disabled: false,
 };
 
 export const Button = (props: Props) => {
-  const { text, type, className, onClick } = props;
+  const { text, type, className, onClick, disabled } = props;
 
   return (
     <button
       className={clsx("btn btn-lg btn-success", className)}
       type={type}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
